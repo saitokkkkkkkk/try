4142caf3fb451cc24c9910fa52296b64f49db3fb
saitokkkkkkkk <k_saito@craid-inc.com>
2024-04-19 00:15:18 +0900

    試し

---
+++ try.php
@@ -0,0 +1,19 @@
+<?php
+
+class MyClass {
+    public $prop;
+
+    public function __construct($value) {
+        $this->prop = $value;
+    }
+
+    public function getProperty() {
+        return $this->prop;
+    }
+}
+
+$obj = new MyClass("Hello");
+echo $obj->getProperty(); // "Hello"を出力
+
+
+?>
